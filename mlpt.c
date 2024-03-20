e3e0c0e1e505beed35deed76dd8c9a94be871b2a
Michael <tvq3bn@virginia.edu>
2024-03-20 04:33:53 -0400

    new commit

--- mlpt.c
+++ mlpt.c
@@ -39,21 +39,21 @@ void page_allocate(size_t va) {
     }
 
     size_t *current_pt = (size_t *)ptbr;
-    for (int level = 1; level < LEVELS; ++level) {
+    for (int level = 1; level <= LEVELS; ++level) {
         size_t index = (va >> INDEX_SHIFT(level)) & INDEX_MASK;
-        current_pt = get_or_create_page_table(current_pt, index);
-    }
-
-    size_t final_index = (va >> INDEX_SHIFT(LEVELS)) & INDEX_MASK;
-    if (!(current_pt[final_index] & VALID_BIT_MASK)) {
-        size_t *data_page;
-        if (posix_memalign((void **)&data_page, PAGE_SIZE, PAGE_SIZE) != 0) {
-            fprintf(stderr, "Memory allocation failed\n");
-            exit(EXIT_FAILURE);
+        if ((level == LEVELS) || !(current_pt[index] & VALID_BIT_MASK)) {
+            size_t *new_page_or_table;
+            if (posix_memalign((void **)&new_page_or_table, PAGE_SIZE, PAGE_SIZE) != 0) {
+                fprintf(stderr, "Memory allocation failed\n");
+                exit(EXIT_FAILURE);
+            }
+            memset(new_page_or_table, 0, PAGE_SIZE);
+            size_t ppn = ((size_t)new_page_or_table) >> POBITS;
+            current_pt[index] = (ppn << POBITS) | VALID_BIT_MASK;
         }
-        memset(data_page, 0, PAGE_SIZE);
-        size_t ppn = ((size_t)data_page) >> POBITS;
-        current_pt[final_index] = (ppn << POBITS) | VALID_BIT_MASK;
+        if (level < LEVELS) {
+            current_pt = (size_t *)(current_pt[index] & ~VALID_BIT_MASK);
+        }
     }
 }
 
